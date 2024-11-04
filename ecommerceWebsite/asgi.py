1fb8b33c484d2ba04512766b1de1e11184caab3d
KubaWaliszewski <159789458+KubaWaliszewski@users.noreply.github.com>
2024-11-04 15:00:58 +0100

    commit asgi.py

--- ecommerceWebsite/asgi.py
+++ ecommerceWebsite/asgi.py
@@ -1,23 +1,21 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerceWebsite.settings')
 
+# Import Channels po ustawieniu Django settings
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from chat import routing
-
-django_asgi_application = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        'http': django_asgi_application,
+        'http': get_asgi_application(),  # Przesunięte do środka ProtocolTypeRouter
         'websocket': AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(routing.websocket_urlpatterns)
             )
-        )
+        ),
     }
 )
