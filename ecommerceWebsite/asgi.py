ccd4e44327b2b83d8fbeb7cd57dd7930521f810a
KubaWaliszewski <159789458+KubaWaliszewski@users.noreply.github.com>
2024-11-04 13:47:24 +0100

    commit test allowedhost

--- ecommerceWebsite/asgi.py
+++ ecommerceWebsite/asgi.py
@@ -14,10 +14,10 @@ django_asgi_application = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         'http': django_asgi_application,
-        'websocket': AllowedHostsOriginValidator(
+        'websocket':
             AuthMiddlewareStack(
                 URLRouter(routing.websocket_urlpatterns)
             )
-        )
+
     }
 )
